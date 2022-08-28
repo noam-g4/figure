76c5e4424470a599d17389f465ab4382bd9216e3
noam-g4 <noam.g4@gmail.com>
2022-08-28 19:26:07 +0300

    fix equal sign in string parsing

--- test/figure_test.go
+++ test/figure_test.go
@@ -10,6 +10,7 @@ import (
 type Conf struct {
 	Env       string `yaml:"env"`
 	WriteMode bool   `yaml:"writeMode"`
+	URI       string `yaml:"uri"`
 	Others    struct {
 		Retries int      `yaml:"retries"`
 		Options []string `yaml:"options"`
@@ -21,6 +22,7 @@ func TestLoadConfig(t *testing.T) {
 	os.Setenv("TST_WRITE_MODE", "TRUE")
 	os.Setenv("TST_RETRIES", "5")
 	os.Setenv("TST_OPTIONS", "[a,b,c]")
+	os.Setenv("TST_URI", "mongodb://admin:pass@docdb-cluster.us-east-1.docdb.amazonaws.com:27017/?retryWrites=false&ssl=true&sslCertificateAuthorityFile=secret/cert_ddb.pem&maxIdleTimeMS=30000")
 
 	err, conf := figure.LoadConfig[Conf](figure.Settings{
 		FilePath:   "./resource/modify-test.yml",
@@ -30,7 +32,7 @@ func TestLoadConfig(t *testing.T) {
 	})
 
 	if err != nil {
-		t.Fail()
+		t.Error(err)
 	}
 
 	if conf.Env != "modified" {
@@ -47,5 +49,9 @@ func TestLoadConfig(t *testing.T) {
 
 	if len(conf.Others.Options) != 3 && conf.Others.Options[1] != "b" {
 		t.Error(conf.Others.Options)
+	}
+
+	if conf.URI != "mongodb://admin:pass@docdb-cluster.us-east-1.docdb.amazonaws.com:27017/?retryWrites=false&ssl=true&sslCertificateAuthorityFile=secret/cert_ddb.pem&maxIdleTimeMS=30000" {
+		t.Error(conf.URI)
 	}
 }
